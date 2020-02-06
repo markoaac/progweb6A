3095cd1f191d2ee6762161d2bca95472012a93a7
markoaac <49350774+markoaac@users.noreply.github.com>
2020-02-05 20:23:20 -0600

    Add files via upload

---
+++ Layout/estilos.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="../Plugins/Bootstrap/css/bootstrap.min.css" />
+<link rel="stylesheet" href="../Plugins/font-awesome/css/font-awesome.css" />
+<link href="../Plugins/BootstrapFileInput/css/fileinput.min.css" rel="stylesheet" type="text/css"/>
+<link rel="stylesheet" href="../Dominus/tempusdominus-bootstrap-4.min.css" />
+<link href="../Plugins/fancybox/jquery.fancybox.css" rel="stylesheet" type="text/css"/>
+<link href="../Plugins/sweetalert/sweetalert.css" rel="stylesheet" type="text/css"/>
+<link href="../Plugins/DataTables/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css"/>
---
+++ Layout/scripts.php
@@ -0,0 +1,12 @@
+<script src="../Plugins/js/jquery-3.0.0.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="../Plugins/Moment/moment.min.js"></script>
+<script src="../Plugins/js/umd/popper.min.js" type="text/javascript"></script>
+<script src="../Plugins/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="../Plugins/BootstrapFileInput/js/fileinput.min.js" type="text/javascript"></script>
+<script src="../Plugins/BootstrapFileInput/js/locales/es.js" type="text/javascript"></script>
+<script type="text/javascript" src="../Plugins/Dominus/tempusdominus-bootstrap-4.min.js"></script>
+<script src="../Plugins/fancybox/jquery.fancybox.js" type="text/javascript"></script>
+<script src="../Plugins/sweetalert/sweetalert.min.js" type="text/javascript"></script>
+<script src="../Plugins/jfvalidator/jquery.form-validator.min.js" type="text/javascript"></script>
+<script src="../Plugins/jfvalidator/es.js" type="text/javascript"></script>
+<script src="../Plugins/DataTables/js/jquery.dataTables.min.js" type="text/javascript"></script>
